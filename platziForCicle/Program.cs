421f9abcc8c1bb7959fdf3b2cc90455266d50491
SekaitsuSistecredito <lbernategordillo@gmail.com>
2022-07-16 12:48:50 -0500

    Platzi Ciclo For

---
+++ platziForCicle/Program.cs
@@ -0,0 +1,14 @@
+﻿//LOS CICLOS FOR EJECUTAN UNA INSTRUCCION O VARIAS, CADA VES QUE SE EJECUTA UN CICLO 
+using System;
+
+namespace platziForCicle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0 /*CONDICION INICIAL*/; i <= 50/*LIMITE DE REPETICIONES*/; i = i + 2/*INCREMENTO*/)
+            Console.WriteLine("Hello, World! #{0}" , i);
+        }
+    }
+}
